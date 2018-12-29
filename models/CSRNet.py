598b86671630b5b38aed3842b9b2fcd2d91edc49
Junyu Gao <gjy3035@gmail.com>
2018-12-29 09:23:37 +0800

    modified csrnet

--- models/CSRNet.py
+++ models/CSRNet.py
@@ -21,7 +21,7 @@ class CSRNet(nn.Module):
         x = self.frontend(x)
         x = self.backend(x)
         x = self.output_layer(x)
-        # x = F.upsample(x,scale_factor=8)
+        x = F.upsample(x,scale_factor=8)
         return x
     def _initialize_weights(self):
         for m in self.modules():
