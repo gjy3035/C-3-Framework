31adb816151b0f237d0084b9370d2c1215c88210
Junyu Gao <gjy3035@gmail.com>
2018-12-30 17:31:40 +0800

    VGG on GCC_rd

--- results_reports/VGG/GCC_rd/config.py
+++ results_reports/VGG/GCC_rd/config.py
@@ -9,7 +9,7 @@ cfg = __C
 
 #------------------------------TRAIN------------------------
 __C.SEED = 3035 # random seed,  for reporduction
-__C.DATASET = 'SHHA' # dataset selection: GCC, SHHA, SHHB, UCF50, QNRF, WE
+__C.DATASET = 'GCC' # dataset selection: GCC, SHHA, SHHB, UCF50, QNRF, WE
 
 if __C.DATASET == 'UCF50':# only for UCF50
 	from datasets.UCF50.setting import cfg_data
@@ -20,7 +20,7 @@ if __C.DATASET == 'GCC':# only for GCC
 	__C.VAL_MODE = cfg_data.VAL_MODE 
 
 
-__C.NET = 'MCNN' # net selection: MCNN, VGG, VGG_DECODER, Res50, CSRNet
+__C.NET = 'VGG' # net selection: MCNN, VGG, VGG_DECODER, Res50, CSRNet
 
 __C.PRE_GCC = False # use the pretrained model on GCC dataset
 __C.PRE_GCC_MODEL = '' # path to model
@@ -28,7 +28,7 @@ __C.PRE_GCC_MODEL = '' # path to model
 __C.GPU_ID = [0] # sigle gpu: [0], [1] ...; multi gpus: [0,1]
 
 # learning rate settings
-__C.LR = 1e-4 # learning rate
+__C.LR = 1e-5 # learning rate
 __C.LR_DECAY = 1 # decay rate
 __C.LR_DECAY_START = -1 # when training epoch is more than it, the learning rate will be begin to decay
 __C.NUM_EPOCH_LR_DECAY = 1 # decay frequency
@@ -54,8 +54,8 @@ __C.EXP_PATH = './exp' # the path of logs, checkpoints, and current codes
 
 
 #------------------------------VAL------------------------
-__C.VAL_DENSE_START = 200
-__C.VAL_FREQ = 10 # Before __C.VAL_DENSE_START epoches, the freq is set as __C.VAL_FREQ
+__C.VAL_DENSE_START = -1
+__C.VAL_FREQ = 5 # Before __C.VAL_DENSE_START epoches, the freq is set as __C.VAL_FREQ
 
 #------------------------------VIS------------------------
 __C.VISIBLE_NUM_IMGS = 1 #  must be 1 for training images with the different sizes
---
+++ results_reports/VGG/GCC_rd/readme.md
@@ -0,0 +1,11 @@
+The results of VGG on GCC dataset using random splitting.
+
+The model is trained 35 epoches, which achieves MAE of **36.6** and MSE of **88.9**. 
+
+## Screenshot of Training Process
+
+![Detialed infomation during the traning phase.](./img1.png "quantitative-results")
+
+## Visualization of Density Map
+
+![Detialed infomation during the traning phase.](./img2.png "visualization")
