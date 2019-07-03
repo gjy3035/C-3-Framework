cacdbb9ebe74dc4db9f74788b5c29c04d54eeeb8
Junyu Gao <gjy3035@gmail.com>
2019-07-03 19:30:23 +0800

    collate

--- datasets/QNRF/loading_data.py
+++ datasets/QNRF/loading_data.py
@@ -51,7 +51,7 @@ def SHHA_collate(batch):
     # @GJY 
     r"""Puts each data field into a tensor with outer dimension batch size"""
 
-    transposed = zip(*batch) # imgs and dens
+    transposed = list(zip(*batch)) # imgs and dens
     imgs, dens = [transposed[0],transposed[1]]
 
 
